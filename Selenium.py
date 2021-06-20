1c03270d2daa40f5b98f8d5bbb989709051c40de
Esmer-byte <77016339+Esmer-byte@users.noreply.github.com>
2021-06-20 18:40:39 +0300

    Update Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -1,70 +1,36 @@
 from selenium import webdriver
 import selenium
-import time, array
+import time
 from selenium.webdriver.common.keys import Keys
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
 
-driver.get("http://localhost:3000/")
+driver.get("http://localhost:3001/")
+maximum = 0
 
-max = 0
-search = driver.find_element_by_name("input")
-search.send_keys("TSLA")
-search = driver.find_element_by_name("submit")
-search.send_keys(Keys.RETURN)
-time.sleep(2)
-search = driver.find_element_by_name("display")
-search.send_keys(Keys.RETURN)
-main = driver.find_element_by_id("4")
-print(main.text)
-driver.find_element_by_name("input").clear()
 
-if float(main.text) > max:
-    max = float(main.text)
+def myFunction(str):
+    global maximum
+    search = driver.find_element_by_name("input")
+    search.send_keys(str)
+    search = driver.find_element_by_name("submit")
+    search.send_keys(Keys.RETURN)
+    time.sleep(2)
+    search = driver.find_element_by_name("display")
+    search.send_keys(Keys.RETURN)
+    main = driver.find_element_by_id("4")
+    print(main.text)
+    driver.find_element_by_name("input").clear()
+    if float(main.text) > maximum:
+        maximum = float(main.text)
 
-search = driver.find_element_by_name("input")
-search.send_keys("IBM")
-search = driver.find_element_by_name("submit")
-search.send_keys(Keys.RETURN)
-time.sleep(2)
-search = driver.find_element_by_name("display")
-search.send_keys(Keys.RETURN)
-main = driver.find_element_by_id("4")
-print(main.text)
-driver.find_element_by_name("input").clear()
 
-if float(main.text) > max:
-    max = float(main.text)
+myFunction("TSLA")
+myFunction("IBM")
+myFunction("FB")
+myFunction("AMZN")
 
-search = driver.find_element_by_name("input")
-search.send_keys("AMZN")
-search = driver.find_element_by_name("submit")
-search.send_keys(Keys.RETURN)
-time.sleep(2)
-search = driver.find_element_by_name("display")
-search.send_keys(Keys.RETURN)
-main = driver.find_element_by_id("4")
-print(main.text)
-driver.find_element_by_name("input").clear()
-
-if float(main.text) > max:
-    max = float(main.text)
-
-search = driver.find_element_by_name("input")
-search.send_keys("FB")
-search = driver.find_element_by_name("submit")
-search.send_keys(Keys.RETURN)
-time.sleep(2)
-search = driver.find_element_by_name("display")
-search.send_keys(Keys.RETURN)
-main = driver.find_element_by_id("4")
-print(main.text)
-driver.find_element_by_name("input").clear()
-
-if float(main.text) > max:
-    max = float(main.text)
-
-print(max)
+print(maximum)
 time.sleep(2)
 driver.quit()
